dc0d767882948e13bf14edf1418a7d1ee981ce3d
infinity-atom <50982527+infinity-atom@users.noreply.github.com>
2022-03-31 19:28:18 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    var menuid = document.getElementById("menu");
+
+    if (menuid.style.display == "none") {
+        menuid.style.display = "block";
+    } else {
+        menuid.style.display = "none";
+    }
+}
